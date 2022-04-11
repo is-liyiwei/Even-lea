c158f2f11b22848faa753aec925a449ec0af4f2a
is-liyiwei <826438633@qq.com>
2022-04-12 00:37:37 +0800

    update

--- dist/assets/root.2c1c9dfd.js
+++
@@ -1,1 +0,0 @@
-import{u as o,L as a,M as s,o as d,d as e,O as t}from"./vendor.d4080a92.js";const n={name:"root",components:{},setup:(a,s)=>(o().push({name:"home",query:{id:"666"}}),{})},r=t();a("data-v-1c01185d");const c={id:"root"};s();const m=r(((o,a,s,t,n,r)=>(d(),e("div",c))));n.render=m,n.__scopeId="data-v-1c01185d";export default n;
